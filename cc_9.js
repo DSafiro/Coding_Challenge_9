3d906a569bc44e4a3a3aa2e46473959150f58296
Dylan <safirodylan@gmail.com>
2025-02-23 00:57:25 -0500

    Task 3 - Created Company Class

--- cc_9.js
+++ cc_9.js
@@ -5,7 +5,7 @@ class Employee {
         this.id = id; // Assigns ID property
         this.department = department; // Assigns department property
         this.salary = salary; // Assigns salary property
-    }; 
+    }; // Creates constructor for Employee class
     getDetails() {
         return `Employee: ${this.name}, ID: ${this.id}, Department: ${this.department}, Salary: $${this.salary}`; // Returns employee details
     }; // Creates method that returns employee details
@@ -22,7 +22,7 @@ class Manager extends Employee {
     constructor (name, id, department, salary, teamSize) {
         super(name, id, department, salary); // Calls previous constructor from Employee class
         this.teamSize = teamSize; // Assigns team size property
-    };
+    }; // Creates constructor for Manager class
     getDetails() {
        return `Manager: ${this.name}, ID: ${this.id}, Department: ${this.department}, Salary: $${this.salary}, Team Size: ${this.teamSize}`; // Returns manager details with updated team size
     }; // Updated getDetails method
@@ -33,3 +33,21 @@ class Manager extends Employee {
 const mgr1 = new Manager("John Smith", 201, "IT", 8000, 5); // Creates new manager object
 console.log(mgr1.getDetails());// Produces expected output of "Manager: John Smith, ID: 201, Department: IT, Salary: $8000, Team Size: 5"
 console.log(mgr1.calculateBonus());  // Produces expected output of 9600
+
+// Task 3: Created Company Class
+class Company {
+    constructor (name, employees) {
+        this.name = name; // Assigns name property
+        this.employees = []; // Assigns employees as array
+    } // Creates constructor for company
+    addEmployee(employee) { 
+        return this.employees.push(employee); // Adds new employees to array
+    }; // Creates a method to add employees
+    listEmployees() {
+        this.employees.forEach(employee => console.log(employee.getDetails())); // Outputs list of employees
+    }; // Creates a method that lists employee
+}; // Declares Company class with attributes
+const company = new Company("TechCorp"); // Creates new company
+company.addEmployee(emp1); // Adds employee to company
+company.addEmployee(mgr1); // Adds manager to company
+company.listEmployees(); // Produces expected output of  "Employee: Alice Johnson, ID: 101, Department: Sales, Salary: $5000" and "Manager: John Smith, ID: 201, Department: IT, Salary: $8000, Team Size: 5"
